60cc93bbc60da05394af34e76f7d54fe2ba48c33
justinembawomye <jmbawomye@gmail.com>
2019-01-29 20:52:57 +0300

    changes have been made

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts =[
+    {
+        "title":"Oraine oota",
+        "content":"webaale"
+    },
+
+  {
+        "title":"Oraine oota",
+        "content":"wasibota"
+    }
+
+]
 
 @app.route('/')
 def home():
-    return "Home page"
+    return render_template("home.html", posts=posts)
